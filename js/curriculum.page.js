434f2cd245c54f22ad40ae0c2c388789247014f3
RuneKockLind <ruli15ab@students.cbs.dk>
2016-12-07 09:18:49 +0100

    Den har ikke haft tilføjet curriculum siderne så derfor tilføjes de nu. De spiller bare

---
+++ js/curriculum.page.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+    //Fires on page-load
+    SDK.Curriculum.getCurriculum(function (err, data) {
+        if (err) {
+            return
+        }
+
+        var decrypted = encryptDecrypt(data);
+        decrypted = JSON.parse(decrypted);
+
+
+        var $curriculumTableBody = $("#curriculumTableBody");
+        decrypted.forEach(function (curriculum, i) {
+
+            $curriculumTableBody.append(
+                "<tr>" +
+                "<td>" + curriculum.curriculumID + "</td>" +
+                "<td>" + curriculum.school + "</td>" +
+                "<td>" + curriculum.education + "</td>" +
+                "<td>" + curriculum.semester + "</td>" +
+                "<td><button class='visBooksButton' data-curriculumId=" + curriculum.curriculumID + "> Se bøger</button></td>" +
+                "</tr>");
+        });
+
+        $(".visBooksButton").on("click", function () {
+            var $button = $(this);
+            var id = $button.data("curriculumid");
+            $("#visBooksModal").modal();
+
+            SDK.Curriculum.getCurriculumBook(id, function (err, data) {
+                if (err) throw err;
+
+                var decrypted = encryptDecrypt(data);
+                decrypted = JSON.parse(decrypted);
+
+                var $CurriculumBookTableBody = $("#CurriculumBookTableBody");
+                decrypted.forEach(function (book, i) {
+
+                    $CurriculumBookTableBody.append(
+                        "<tr>" +
+                        "<td>" + book.title + "</td>" +
+                        "<td>" + book.author + "</td>" +
+                        "<td>" + book.publisher + "</td>" +
+                        "<td>" + book.version + "</td>" +
+                        "<td>" + book.priceAB + ".-</td>" +
+                        "<td>" + book.priceSAXO + ".-</td>" +
+                        "<td>" + book.priceCDON + ".-</td>" +
+                        "</tr>");
+
+                });
+                $("#closeModal").on("click", function () {
+                    $("#CurriculumBookTableBody").children().remove()
+                });
+            });
+
+        });
+
+    })
+})
+
